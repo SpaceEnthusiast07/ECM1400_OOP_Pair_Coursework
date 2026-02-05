7b36f16023428c55a4703b855d35fcbac816a346
Cameron <chrussellpredator07@gmail.com>
2026-02-05 16:57:27 +0000

    Continued work on unit class.

--- src/main/java/cityrescue/Unit.java
+++ src/main/java/cityrescue/Unit.java
@@ -1,6 +1,11 @@
 package cityrescue;
 
-public class Unit {
+public abstract class Unit {
     // Declare a variable to hold the unit id
-    int unitId;
+    private int unitId;
+
+    // Getter method for unit id
+    public int getUnitId() {
+        return unitId;
+    }
 }
