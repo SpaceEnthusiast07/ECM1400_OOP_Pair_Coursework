a6d830c48704552a042c3abf8a5269c98db206a7
Cameron <chrussellpredator07@gmail.com>
2026-02-05 17:07:57 +0000

    Added comments to Station class and completed setStationCapacity function.

--- src/main/java/cityrescue/Station.java
+++ src/main/java/cityrescue/Station.java
@@ -1,25 +1,30 @@
 package cityrescue;
 
 public class Station {
+    // Declared variable to hold station name
     private String name;
-    final private int maxUnits = 50;
+    // Variable to hold the maximum number of units at this station
+    private int maxUnits = 50;
+    // Variable represents the location of this station on the city map
     private int[] coords = new int[2];
-
+    // Constant used to keep track of the number of stations
     private static int numberOfStations = 0;
-    private int stationId;
+    // Unique id for each station
+    private final int stationId;
 
-
-
+    // Constructor
     public Station(String name, int x, int y) {
         this.name = name;
         this.coords = new int[] {x,y};
         this.stationId = ++numberOfStations;
     }
 
-    public void setStationCapacity(int stationId, int maxUnits) {
-
+    // Sets the maximum number of units this station can hold
+    public void setStationCapacity(int maxUnits) {
+        this.maxUnits = maxUnits;
     }
 
+    // Returns this station's id
     public int getStationId() {
         return this.stationId;
     }
