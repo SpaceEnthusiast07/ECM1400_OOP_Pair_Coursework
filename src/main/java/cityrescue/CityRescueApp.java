f417f005215bc7d5a040b98b190120332801a672
Cameron <chrussellpredator07@gmail.com>
2026-03-03 20:16:24 +0000

    Completed CityRescueApp and the simulation outcomes are what I am predicting! However, it needs a quick verify, just to check.

--- src/main/java/cityrescue/CityRescueApp.java
+++ src/main/java/cityrescue/CityRescueApp.java
@@ -4,6 +4,7 @@ import cityrescue.enums.IncidentType;
 import cityrescue.enums.UnitType;
 import cityrescue.exceptions.*;
 
+import javax.swing.plaf.basic.BasicPasswordFieldUI;
 import java.util.Scanner;
 
 /**
@@ -44,7 +45,7 @@ public class CityRescueApp {
             sim.initialise(8, 8);
 
             // Output the simulation
-            System.out.println("Started simulation with a city map of size "+x+"x"+y+".\n");
+            System.out.println("Started simulation with a city map of size "+x+"x"+y+".");
         } catch (InvalidGridException e) {
             System.out.println("ERROR: Width or height are invalid.");
             System.exit(1);
@@ -57,13 +58,13 @@ public class CityRescueApp {
         }
         //endregion
 
-        pauseDemonstration();
+        pauseDemonstration("add 4 obstacles");
 
-        //region Add 3 Random Obstacles
+        //region Add 4 Random Obstacles
         // Add obstacle 1
         try {
             sim.addObstacle(2,6);
-            System.out.println("Added: Obstacle at location (2,6).\n");
+            System.out.println("Added: Obstacle at location (2,6).");
         } catch (InvalidLocationException e) {
             System.out.println("ERROR: Invalid location to place obstacle.");
         }
@@ -71,7 +72,7 @@ public class CityRescueApp {
         // Add obstacle 2
         try {
             sim.addObstacle(5,6);
-            System.out.println("Added: Obstacle at location (5,6).\n");
+            System.out.println("Added: Obstacle at location (5,6).");
         } catch (InvalidLocationException e) {
             System.out.println("ERROR: Invalid location to place obstacle.");
         }
@@ -79,13 +80,21 @@ public class CityRescueApp {
         // Add obstacle 3
         try {
             sim.addObstacle(1,1);
-            System.out.println("Added: Obstacle at location (1,1).\n");
+            System.out.println("Added: Obstacle at location (1,1).");
+        } catch (InvalidLocationException e) {
+            System.out.println("ERROR: Invalid location to place obstacle.");
+        }
+
+        // Add obstacle 4
+        try {
+            sim.addObstacle(4,4);
+            System.out.println("Added: Obstacle at location (4,4).");
         } catch (InvalidLocationException e) {
             System.out.println("ERROR: Invalid location to place obstacle.");
         }
         //endregion
 
-        pauseDemonstration();
+        pauseDemonstration("add 1 station");
 
         //region Add 1 Station to Sim
         // Terminates the loop once the user has input correct station information
@@ -113,7 +122,7 @@ public class CityRescueApp {
                 validStation = true;
 
                 // Output this station
-                System.out.println("Added: Station called '"+stationName+"' at location ("+x+","+y+") with ID "+station1Id+".\n");
+                System.out.println("Added: Station called '"+stationName+"' at location ("+x+","+y+") with ID "+station1Id+".");
             } catch (InvalidNameException e) {
                 System.out.println("ERROR: Station name is invalid.");
             } catch (InvalidLocationException e) {
@@ -125,7 +134,7 @@ public class CityRescueApp {
         if (numberOfAttempts == 10) System.exit(1);
         //endregion
 
-        pauseDemonstration();
+        pauseDemonstration("add 3 units");
 
         //region Add 3 Units to Sim
         // Add 3 units to the simulation
@@ -133,17 +142,17 @@ public class CityRescueApp {
             // Add an Ambulance to the simulation
             int ambulanceId = sim.addUnit(station1Id, UnitType.AMBULANCE);
             // Output this new unit
-            System.out.println("Added: Ambulance unit with a home station ID of "+station1Id+".\n");
-
-            // Add a Fire_Engine to the simulation
-            int fireEngineId = sim.addUnit(station1Id, UnitType.FIRE_ENGINE);
-            // Output this new unit
-            System.out.println("Added: Fire engine unit with a home station ID of "+station1Id+".\n");
+            System.out.println("Added: Ambulance unit with a home station ID of "+station1Id+".");
 
             // Add a Police_Car to the simulation
             int policeCarId = sim.addUnit(station1Id, UnitType.POLICE_CAR);
             // Output this new unit
-            System.out.println("Added: Police car unit with a home station ID of "+station1Id+".\n");
+            System.out.println("Added: Police car unit with a home station ID of "+station1Id+".");
+
+            // Add a Fire_Engine to the simulation
+            int fireEngineId = sim.addUnit(station1Id, UnitType.FIRE_ENGINE);
+            // Output this new unit
+            System.out.println("Added: Fire engine unit with a home station ID of "+station1Id+".");
         } catch (IDNotRecognisedException e) {
             System.out.println("ERROR: Provided station ID doesn't exist.");
         } catch (InvalidUnitException e) {
@@ -153,7 +162,7 @@ public class CityRescueApp {
         }
         //endregion
 
-        pauseDemonstration();
+        pauseDemonstration("add 3 incidents");
 
         //region Add 3 Incidents to Sim
         // Add a medical incident
@@ -161,7 +170,7 @@ public class CityRescueApp {
             // Add an incident of type medical
             int medicalId = sim.reportIncident(IncidentType.MEDICAL, 3, 5, 8);
             // Output this incident
-            System.out.println("Added: Medical incident with severity 3, location (5,8) and ID "+medicalId+".\n");
+            System.out.println("Added: Medical incident with severity 3, location (5,8) and ID "+medicalId+".");
         } catch (InvalidSeverityException e) {
             System.out.println("ERROR: Medical severity must be between 1 and 5.");
         } catch (InvalidLocationException e) {
@@ -173,7 +182,7 @@ public class CityRescueApp {
             // Add an incident of type fire
             int fireId = sim.reportIncident(IncidentType.FIRE, 5, 1, 3);
             // Output this incident
-            System.out.println("Added: Fire incident with severity 5, location (1,3) and ID "+fireId+".\n");
+            System.out.println("Added: Fire incident with severity 5, location (1,3) and ID "+fireId+".");
         } catch (InvalidSeverityException e) {
             System.out.println("ERROR: Fire severity must be between 1 and 5.");
         } catch (InvalidLocationException e) {
@@ -185,7 +194,7 @@ public class CityRescueApp {
             // Add an incident of type crime
             int crimeId = sim.reportIncident(IncidentType.CRIME, 2, 6, 2);
             // Output this incident
-            System.out.println("Added: Crime incident with severity 2, location (6,2) and ID "+crimeId+".\n");
+            System.out.println("Added: Crime incident with severity 2, location (6,2) and ID "+crimeId+".");
         } catch (InvalidSeverityException e) {
             System.out.println("ERROR: Crime severity must be between 1 and 5.");
         } catch (InvalidLocationException e) {
@@ -193,31 +202,86 @@ public class CityRescueApp {
         }
         //endregion
 
-        pauseDemonstration();
+        pauseDemonstration("output simulation status");
 
         outputSimStatus();
 
-        pauseDemonstration();
+        pauseDemonstration("dispatch units");
 
         //region Dispatch Units
-        System.out.println("Dispatch Units:");
         sim.dispatch();
-        System.out.println("Units Dispatched.\n");
+        System.out.println("Units dispatched.");
         //endregion
 
-        pauseDemonstration();
+        pauseDemonstration("output simulation status");
 
         outputSimStatus();
 
-        pauseDemonstration();
+        pauseDemonstration("advance the simulation by 1 tick");
 
         //region Advance Simulation by 1 Tick
-        System.out.println("Advance Simulation by 1 Tick:");
         sim.tick();
-        System.out.println("Simulation Advanced.\n");
+        System.out.println("Simulation advanced.");
         //endregion
 
-        pauseDemonstration();
+        pauseDemonstration("output simulation status");
+
+        outputSimStatus();
+
+        pauseDemonstration("skip to TICK=3 when I predict U3 to arrive at scene");
+
+        //region Skip to TICK=3 for U3 to arrive at scene
+        sim.tick();
+        sim.tick();
+        System.out.println("Advanced sim by 2 ticks.");
+        //endregion
+
+        pauseDemonstration("output simulation status");
+
+        outputSimStatus();
+
+        pauseDemonstration("skip to TICK=5 for when I predict U2 to arrive at scene");
+
+        //region Skip to TICK=5 for U2 to arrive at scene
+        sim.tick();
+        sim.tick();
+        System.out.println("Advanced sim by 2 ticks.");
+        //endregion
+
+        pauseDemonstration("output simulation status");
+
+        outputSimStatus();
+
+        pauseDemonstration("advance another tick for when I predict U1 to arrive at scene");
+
+        //region Advance another tick for U1 to arrive at scene
+        sim.tick();
+        System.out.println("Advanced simulation by 1 tick.");
+        //endregion
+
+        pauseDemonstration("output simulation status");
+
+        outputSimStatus();
+
+        pauseDemonstration("advance sim by 1 tick for when I predict U3 to complete its incident");
+
+        //region Advance sim by 1 tick
+        sim.tick();
+        System.out.println("Advanced sim by 1 tick.");
+        //endregion
+
+        pauseDemonstration("output simulation status");
+
+        outputSimStatus();
+
+        pauseDemonstration("advance sim by 1 tick for when I predict U1 and U2 to complete their incident");
+
+        //region Advance sim by 1 tick
+        sim.tick();
+        System.out.println("Advanced sim by 1 tick.");
+        //endregion
+
+        pauseDemonstration("output simulation status");
 
         outputSimStatus();
 
@@ -237,18 +301,25 @@ public class CityRescueApp {
      * Output the status of the simulation.
      */
     static void outputSimStatus() {
-        System.out.println("Simulation Status:");
         System.out.println(sim.getStatus());
     }
 
     /**
-     * Waits for the user to press the space bar, allowing them to explain part of the demonstration.
+     * Waits for the user to press enter, allowing them to explain part of the demonstration.
      */
     static void pauseDemonstration() {
         // Pauses demonstration for explanation
-        System.out.print("Press enter to continue... ");
+        System.out.print("\n\nPress enter to continue... ");
         scanner.nextLine();
-        System.out.println();
-        System.out.println();
+    }
+
+    /**
+     * Waits for the user to press enter, allowing them to explain part of the demonstration.
+     * @param message Allows the client to output a custom pause demonstration message.
+     */
+    static void pauseDemonstration(String message) {
+        // Pauses demonstration for explanation
+        System.out.print("\n\n--- Press enter to " + message + "... ");
+        scanner.nextLine();
     }
 }
